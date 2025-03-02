8f8e07a030c92bf8596aa37237f85f1fb8768547
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2025-03-02 16:19:34 +0100

    qubes: several fixes

--- ansible_module/conns/qubes.py
+++ ansible_module/conns/qubes.py
@@ -57,7 +57,6 @@ import subprocess
 from ansible.module_utils.common.text.converters import to_bytes
 from ansible.plugins.connection import ConnectionBase, ensure_connect
 
-
 try:
     from __main__ import display
 except ImportError:
@@ -66,11 +65,12 @@ except ImportError:
     display = Display()
 
 
-# this _has to be_ named Connection
 class Connection(ConnectionBase):
-    """This is a connection plugin for qubes: it uses qubes-run-vm binary to interact with the containers."""
+    """
+    This connection plugin for Qubes OS uses the qvm-run executable
+    to interact with QubesVMs.
+    """
 
-    # String used to identify this Connection class from other classes
     transport = "qubes"
     has_pipelining = True
 
@@ -78,121 +78,119 @@ class Connection(ConnectionBase):
         super(Connection, self).__init__(
             play_context, new_stdin, *args, **kwargs
         )
-
         self._remote_vmname = self._play_context.remote_addr
         self._connected = False
-        # Default username in Qubes
-        self.user = "user"
-        if self._play_context.remote_user:
-            self.user = self._play_context.remote_user
-
-    def _qubes(self, cmd=None, in_data=None, shell="qubes.VMShell"):
-        """run qvm-run executable
-
-        :param cmd: cmd string for remote system
-        :param in_data: data passed to qvm-run-vm's stdin
-        :return: return code, stdout, stderr
-        """
-        display.vvvv("CMD: ", cmd)
-        if not cmd.endswith("\n"):
-            cmd = cmd + "\n"
-        local_cmd = []
-
-        # For dom0
-        local_cmd.extend(["qvm-run", "--pass-io", "--service"])
-        if self.user != "user":
-            # Means we have a remote_user value
-            local_cmd.extend(["-u", self.user])
-
-        local_cmd.append(self._remote_vmname)
-
-        local_cmd.append(shell)
-
-        local_cmd = [
-            to_bytes(i, errors="surrogate_or_strict") for i in local_cmd
-        ]
-
-        display.vvvv("Local cmd: ", local_cmd)
-
-        display.vvv("RUN %s" % (local_cmd,), host=self._remote_vmname)
-        p = subprocess.Popen(
-            local_cmd,
-            shell=False,
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
+        # Use the provided remote_user if set; otherwise default to "user".
+        self.user = (
+            self._play_context.remote_user
+            if self._play_context.remote_user
+            else "user"
         )
 
-        # Here we are writing the actual command to the remote bash
-        p.stdin.write(to_bytes(cmd, errors="surrogate_or_strict"))
-        stdout, stderr = p.communicate(input=in_data)
-        return p.returncode, stdout, stderr
+    def _qubes(
+        self, cmd: str, in_data: bytes = None, shell: str = "qubes.VMShell"
+    ):
+        """
+        Execute a command in the qube via qvm-run.
+
+        :param cmd: Command string to execute on the remote system.
+        :param in_data: Additional data to pass to the remote command's stdin.
+        :param shell: Service type (e.g. qubes.VMShell or qubes.VMRootShell).
+        :return: Tuple of (returncode, stdout, stderr).
+        """
+        display.vvvv(f"CMD: {cmd}")
+        if not cmd.endswith("\n"):
+            cmd += "\n"
+
+        local_cmd = ["qvm-run", "--pass-io", "--service"]
+        if self.user != "user":
+            local_cmd.extend(["-u", self.user])
+        local_cmd.extend([self._remote_vmname, shell])
+        local_cmd_bytes = [
+            to_bytes(arg, errors="surrogate_or_strict") for arg in local_cmd
+        ]
+        display.vvvv(f"Local cmd: {local_cmd_bytes}")
+        display.vvv(f"RUN {local_cmd_bytes}", host=self._remote_vmname)
+
+        # Combine the command and any additional input data
+        combined_input = to_bytes(cmd, errors="surrogate_or_strict")
+        if in_data:
+            combined_input += in_data
+
+        try:
+            result = subprocess.run(
+                local_cmd_bytes,
+                input=combined_input,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                check=False,
+            )
+        except Exception as e:
+            display.error(f"Error executing command via qvm-run: {e}")
+            raise
+
+        return result.returncode, result.stdout, result.stderr
 
     def _connect(self):
-        """No persistent connection is being maintained."""
+        """
+        Establish the connection (no persistent connection is maintained).
+        """
         super(Connection, self)._connect()
         self._connected = True
 
     @ensure_connect
     def exec_command(self, cmd, in_data=None, sudoable=False):
-        """Run specified command in a running QubesVM"""
-        super(Connection, self).exec_command(
-            cmd, in_data=in_data, sudoable=sudoable
+        """
+        Run the specified command in the QubesVM.
+
+        :param cmd: Command to run.
+        :param in_data: Data to send to stdin.
+        :param sudoable: Not used in this plugin.
+        :return: Tuple (returncode, stdout, stderr).
+        """
+        display.vvvv(f"CMD IS: {cmd}")
+        rc, stdout, stderr = self._qubes(cmd, in_data)
+        display.vvvvv(
+            f"STDOUT {stdout!r} STDERR {stderr!r}", host=self._remote_vmname
         )
-
-        display.vvvv("CMD IS: %s" % cmd)
-
-        rc, stdout, stderr = self._qubes(cmd)
-
-        display.vvvvv("STDOUT %r STDERR %r" % (stderr, stderr))
         return rc, stdout, stderr
 
     def put_file(self, in_path, out_path):
-        """Place a local file located in 'in_path' inside VM at 'out_path'"""
-        super(Connection, self).put_file(in_path, out_path)
-        display.vvv(
-            "PUT %s TO %s" % (in_path, out_path), host=self._remote_vmname
-        )
-
+        """
+        Copy a local file from 'in_path' to the remote VM at 'out_path'.
+        """
+        display.vvv(f"PUT {in_path} TO {out_path}", host=self._remote_vmname)
         with open(in_path, "rb") as fobj:
             source_data = fobj.read()
 
-        retcode, dummy, dummy = self._qubes(
-            'cat > "{0}"\n'.format(out_path), source_data, "qubes.VMRootShell"
+        # Try using VMRootShell first; fallback to VMShell if needed.
+        retcode, _, _ = self._qubes(
+            f'cat > "{out_path}"\n', source_data, shell="qubes.VMRootShell"
         )
-        # if qubes.VMRootShell service not supported, fallback to qubes.VMShell and
-        # hope it will have appropriate permissions
         if retcode == 127:
-            retcode, dummy, dummy = self._qubes(
-                'cat > "{0}"\n'.format(out_path), source_data
-            )
-
+            retcode, _, _ = self._qubes(f'cat > "{out_path}"\n', source_data)
         if retcode != 0:
-            raise RuntimeError("Failed to put_file to {0}".format(out_path))
+            raise RuntimeError(f"Failed to put_file to {out_path}")
 
     def fetch_file(self, in_path, out_path):
-        """Obtain file specified via 'in_path' from the container and place it at 'out_path'"""
-        super(Connection, self).fetch_file(in_path, out_path)
-        display.vvv(
-            "FETCH %s TO %s" % (in_path, out_path), host=self._remote_vmname
-        )
-
-        # We are running in dom0
-        cmd_args_list = [
+        """
+        Retrieve a file from the remote VM located at 'in_path' and save it to 'out_path'.
+        """
+        display.vvv(f"FETCH {in_path} TO {out_path}", host=self._remote_vmname)
+        cmd_args = [
             "qvm-run",
             "--pass-io",
             self._remote_vmname,
-            "cat {0}".format(in_path),
+            f"cat {in_path}",
         ]
         with open(out_path, "wb") as fobj:
-            p = subprocess.Popen(cmd_args_list, shell=False, stdout=fobj)
-            p.communicate()
-            if p.returncode != 0:
-                raise RuntimeError(
-                    "Failed to fetch file to {0}".format(out_path)
-                )
+            result = subprocess.run(cmd_args, stdout=fobj)
+        if result.returncode != 0:
+            raise RuntimeError(f"Failed to fetch file to {out_path}")
 
     def close(self):
-        """Closing the connection"""
+        """
+        Close the connection.
+        """
         super(Connection, self).close()
         self._connected = False
