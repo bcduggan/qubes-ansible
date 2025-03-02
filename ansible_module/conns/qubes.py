5d6a947db50ebdcb21d7a97293397a8ca4a30eae
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2025-03-02 15:51:58 +0100

    Update license headers

--- ansible_module/conns/qubes.py
+++ ansible_module/conns/qubes.py
@@ -1,10 +1,23 @@
-# Based on the buildah connection plugin
 # Copyright (c) 2017 Ansible Project
-#               2018 Kushal Das
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# Copyright (C) 2018 Kushal Das
+# Copyright (C) 2025 Frédéric Pierret (fepitre) <frederic@invisiblethingslab.com>
 #
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
-# Written by: Kushal Das (https://github.com/kushaldas)
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# Based on the buildah connection plugin
 
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
--- ansible_module/qubesos.py
+++ ansible_module/qubesos.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-# Copyright: (c) 2018
-# Kushal Das <mail@kushaldas.in>
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# Copyright (c) 2017 Ansible Project
+# Copyright (C) 2018 Kushal Das
+# Copyright (C) 2025 Frédéric Pierret (fepitre) <frederic@invisiblethingslab.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
