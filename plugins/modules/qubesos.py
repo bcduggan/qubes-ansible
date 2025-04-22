63a4e4a32ded2d218bc52ff0bc93a26284b162db
Brian Duggan <brian@dugga.net>
2025-04-22 16:39:29 -0400

    Only change label of cloned qube for standalone creation
    Standalone qube creation is actually a clone operation. Previously, the
    qubesos module would update the new qube's label when the guest qube
    was either a standalone or template qube.
    
    When the guest is a template qube (vmtype: TemplateVM), the module
    shouldn't update the label after cloning.

--- plugins/modules/qubesos.py
+++ plugins/modules/qubesos.py
@@ -319,7 +319,8 @@ class QubesVirt(object):
             vm.netvm = network_vm
         elif vmtype in ["StandaloneVM", "TemplateVM"] and template_vm:
             vm = self.app.clone_vm(template_vm, vmname, vmtype, ignore_errors=(self.app.local_name != "dom0"))
-            vm.label = label
+            if vmtype == "StandaloneVM":
+                vm.label = label
         return 0
 
     def start(self, vmname):
